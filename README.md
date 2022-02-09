7f0f49d1f6b198697b1c6c6150ac30b31ca528e1
Gvico <42806519+Gvico@users.noreply.github.com>
2022-02-09 08:31:31 +0100

    Removed website
    GitHub doesn't support PHP

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # AT-Blaster
-<img src="https://raw.githubusercontent.com/Gvico/AT-Blaster/main/docs/images/logo.png" alt="Logo" height="100" />
+<img src="https://raw.githubusercontent.com/Gvico/AT-Blaster/main/images/logo.png" alt="Logo" height="100" />
 Lasergame Project for BA2 Electronics
 
 (README coming soon)
--- docs/pages/accueil.php
+++
@@ -1,9 +0,0 @@
-<div class=block>
-    <p>AT-Blaster est un projet de BA2 en Electronique Appliquée.</p>
-	<p>Son but est de recréer un système de lasergame personnalisé.</p>
-	<p>Les liens de navigation à gauche vous permettront d'en apprendre plus !</p>
-	<br>
-	<div class=header>
-		<audio controls src="./media/music.mp3"></audio>
-	</div>
-</div>
